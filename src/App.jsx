4474356b22a7458228fe7a98b6d26b364572176c
unknown <micky.mozzo@gmail.com>
2022-11-05 14:50:33 +0200

    boyyy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import "./App.css";
+import Nav from "./components/Nav";
+import SideBar from "./components/SideBar";
 
 function App() {
-
-  return (
-    "Hello World!"
-  )
+    return (
+        <div style={{ height: "120vh" }}>
+            <Nav />
+			<SideBar></SideBar>
+            <div style={{ paddingTop: "3rem" }}>Hello world</div>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/SideBar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+export default function SideBar(props) {
+    return <aside className="side-bar">
+
+    </aside>
+}
