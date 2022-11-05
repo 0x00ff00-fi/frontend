b8dd325ea235966540f7b6afefcc9497eea741a8
Soy Park <soypark@Soys-MacBook-Pro.local>
2022-11-05 15:56:54 +0200

    nav styling

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function Nav(props) {
     return <nav className="nav-bar">
         <div className="nav_logo">Logo here</div>
-        <input className="nav_search" type="text" name="" id="" placeholder="search something"/>
+        <input className="nav_srch" type="text" name="" id="" placeholder="search something"/>
         <div className="nav_user">Huuuman</div>
     </nav>;
 }
