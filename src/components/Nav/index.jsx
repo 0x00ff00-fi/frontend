7d6b129633a495ac1c1580cc963fb33a8f5e9be6
Soy Park <soypark@Soys-MacBook-Pro.local>
2022-11-05 15:47:51 +0200

    nav bar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./styles.css";
 
 export default function Nav(props) {
-    return <nav className="nav-bar">Hello</nav>;
+    return <nav className="nav-bar">
+        <div className="nav_logo">Logo here</div>
+        <input className="nav_search" type="text" name="" id="" placeholder="search something"/>
+        <div className="nav_user">Huuuman</div>
+    </nav>;
 }
