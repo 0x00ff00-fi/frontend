2a1397bbb749e4df9da98d30bf31c6bc6a96b9e9
unknown <micky.mozzo@gmail.com>
2022-11-05 21:12:01 +0200

    search field

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function Nav(props) {
-
-	console.log(document.getElementById("search-field").value)
-
   return (
     <nav className="nav-bar">
       <a href="#" className="nav_logo">
         0x00ff00
       </a>
-      <form /* onSubmit={props?.search(document.getElementById("search-field").value)} */>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          props?.search(document.getElementById("search-field")?.value);
+        }}
+      >
         <input
           className="nav_srch"
           type="text"
