ca0d4465a09832da5886186987fe5c3b23ddfd1f
Soy Park <soypark@Soys-MacBook-Pro.local>
2022-11-05 16:01:58 +0200

    side bar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -3,6 +3,9 @@ import "./styles.css";
 
 export default function SideBar(props) {
     return <aside className="side-bar">
-
+        <ul className="side_menu">
+            <li className="side_itm"><a href="" className="side_link">#explore</a></li>
+            <li className="side_itm"><a href="" className="side_link">settings</a></li>
+        </ul>
     </aside>
 }
